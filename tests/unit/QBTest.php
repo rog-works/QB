bac96845671bfc9abca1da54566b1dfbc6732b77
rog-works <rog_works@yahoo.co.jp>
2018-04-01 13:16:44 +0900

    test: impl tests

--- tests/unit/QBTest.php
+++ tests/unit/QBTest.php
@@ -8,6 +8,29 @@ use QBTest\helpers\TestHelper;
 class QBTest extends \PHPUnit\Framework\TestCase {
 	/**
 	 * @test
+	 * @dataProvider parseData
+	 */
+	public function parse(array $args, array $expected) {
+		$this->assertEquals($expected, TestHelper::invokeMethod(new QB, 'parse', [$args]));
+	}
+
+	public function parseData(): array {
+		return [
+			[['select', ['u.id', 'u.name', 'u.created']], ['select', 'u.id,u.name,u.created']],
+			[['from', 'users'], ['from', 'users']],
+			[['as', 'u'], ['as', 'u']],
+			[['join', 'profiles'], ['join', 'profiles']],
+			[['on', 'p.user_id = u.id'], ['on', 'p.user_id = u.id']],
+			[['join', (new QB)->select(['f.user_id', 'count(*) as follows'])->from('follows')->as('f')->group_by('f.user_id')], ['join', '(SELECT f.user_id,count(*) as follows FROM follows AS f GROUP BY f.user_id)']],
+			[['or', (new QB('u.created < now()'))->and('p.gender = :gender')],['or', '(u.created < now() AND p.gender = :gender)']],
+			[['in', new QB([1, 2, 3])],['in', '(1,2,3)']],
+			[['order','by', ['p.gender', 'u.id desc']],['order', 'by', 'p.gender,u.id desc']],
+			[['limit', [0, 10]],['limit', '0,10']],
+		];
+	}
+
+	/**
+	 * @test
 	 * @dataProvider calcRouteData
 	 */
 	public function calcRoute(array $params, string $name, string $expected) {
